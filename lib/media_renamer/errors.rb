faba1148af9470dec8c64b229588db77d365d285
Przemek Lusar <lluzak@gmail.com>
2014-10-23 22:52:31 +0200

    Introduces configuration holder

--- lib/media_renamer/errors.rb
+++ lib/media_renamer/errors.rb
@@ -2,4 +2,5 @@ module MediaRenamer
   Error                 = Class.new(StandardError)
   UnknownMediaTypeError = Class.new(Error)
   SourceNotExistError   = Class.new(Error)
+  ConfigFileError       = Class.new(Error)
 end
